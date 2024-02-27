db269515d48fa9d97b54a0e4aab2d6f12ebb41a3
Nash <nashb1323@gmail.com>
2024-02-26 19:21:36 -0600

    init setup

--- site.config.ts
+++ site.config.ts
@@ -2,14 +2,14 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'PROJECT-BLOG-HOMEPAGE-d93d00e0e7ce4cc5b9e862f6f6a7527e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: "Nash's Project Blog",
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
   author: 'Travis Fischer',
 
